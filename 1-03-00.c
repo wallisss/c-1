9fcfa0d16e2d8cb12e04790d14b549356df969ef
Wallisss <visa.daho@gmail.com>
2020-05-06 17:01:09 +0300

    add input output

---
+++ 1-03-00.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int radius;   //The distance from outside to center of the circle
+    printf("\nEnter radius: ",radius);
+    scanf("%i" ,&radius)	;   //addres of operator
+    
+    printf("\nHello Caleb Curry World! \n");
+    printf("\nRadius: %i",radius);
+       
+
+  return 0;  
+}
+
