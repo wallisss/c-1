07c3041207487a9c7c5b9384e9dec01287e57ee0
Wallisss <visa.daho@gmail.com>
2020-05-06 17:25:29 +0300

    01:09:54 - C Basics Part 4- CC1-09.c (time of example from video)

--- CC1-09.c
+++ CC1-09.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-    double radius;   //The distance from outside to center of the circle
-    printf("\nEnter radius x,xx: ",radius);
+    int radius;   //The distance from outside to center of the circle
+    printf("\nEnter radius : ",radius);
     scanf("%i" ,&radius)	;   //address of operator
     
     //int area=3.14159*(radius*radius); //logical error
     double area=3.14159*(radius*radius); 
     printf("\nHello Caleb Curry World! \n");
-    printf("\nRadius: %f",area);
+    printf("\nThe area of a circle with radius %i is : %f\n\n",radius,area);
        
 
   return 0;  
