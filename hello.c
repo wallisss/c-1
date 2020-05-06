499920c954000a063c0e022683ab999d8859cce9
Wallisss <visa.daho@gmail.com>
2020-05-06 16:44:41 +0300

    add output

--- hello.c
+++ hello.c
@@ -2,9 +2,12 @@
 
 int main()
 {
-    printf("Hello C World!\n");
-    printf("%i ",9001);   //for integer
-
+    int x=10;
+    int y=x/2;
+    
+    printf("Hello C World! ");
+    printf("%i \n",9001);   //for integer
+    printf("The magic number is: %i\nThe Value of x is %i\n", y,x);
 
   return 0;  
 }
