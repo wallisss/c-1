a425509faeb2d1d7fad95e4a90e1350e70e74698
Wallisss <visa.daho@gmail.com>
2020-05-07 16:11:05 +0300

    ex>>02:10:50 - Int, Float, and Double Data Types

---
+++ NumericDataType.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+        //int double float
+        int dogs=6;
+
+        //floating point
+        //precision -- how much data thr varible can hold
+
+        printf("\nint:%i double:%f  float:%f \n",dogs,1111.1111,1111.1111F);//float ..it become double
+                        //double tqake up twice as much space
+                        //float can not trust
+    
+    
+    
+    return 0;
+}
