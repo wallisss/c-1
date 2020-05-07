40eab486c72227a34cbe57400d1ab8f199503b96
Wallisss <visa.daho@gmail.com>
2020-05-07 13:58:04 +0300

    01:22:49 - C Basics Part 6 exercise 2

--- ComplementMachine.c
+++ ComplementMachine.c
@@ -3,8 +3,9 @@
 int main()
 {
         char name[31]; 
+        printf("Yo gurl enter yo name: ");
         scanf("%s",name);   //int x; &x (address)
-        printf("Hello %s World!!\n",name );//%s string of charecters.
+        printf("Hello %s World!!\nYou look very nice today\n",name );//%s string of charecters.
 
     
     
