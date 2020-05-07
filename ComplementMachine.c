4c231f41d8c134572fcf1af68a873f1d0b9b7058
Wallisss <visa.daho@gmail.com>
2020-05-07 13:54:25 +0300

    01:22:49 - C Basics Part 6 exercise 1

---
+++ ComplementMachine.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+        char name[31]; 
+        scanf("%s",name);   //int x; &x (address)
+        printf("Hello %s World!!\n",name );//%s string of charecters.
+
+    
+    
+    
+    
+    
+    return 0;
+}
