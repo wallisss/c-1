a230d8da3098befe69de144d70784e26a63556ce
Wallisss <visa.daho@gmail.com>
2020-05-08 16:28:32 +0300

    02:43:17 - _Bool Data Type

---
+++ Bool.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    // boolen 0=fals 1=true
+    _Bool calebIsFat=9001;   //boolean logic ;camel case (other _Bool caleb_is_fat)
+    printf("Is Caleb Fat (1-is yes, 0 is no)? %i\n ",calebIsFat);
+
+    return 0;
+}
