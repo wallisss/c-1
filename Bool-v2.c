7b4ec89842ce1b650d80eb825ae39b4b8aa38b36
Wallisss <visa.daho@gmail.com>
2020-05-09 07:50:22 +0300

    add bool-v2

---
+++ Bool-v2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    // boolen 0=fals 1=true
+    bool calebIsFat=false ;   //or false
+    printf("Is Caleb Fat (1-is yes, 0 is no)? %i\n ",calebIsFat);
+    printf("%i\n", calebIsFat+10);
+    return 0;
+}
