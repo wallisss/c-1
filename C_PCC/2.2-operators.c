ae1619ad6ae40880a0a8f924ed4be9134263c3d8
Wallisss <visa.daho@gmail.com>
2020-09-15 07:46:34 +0300

    temp

---
+++ C_PCC/2.2-operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*  operator (unary) + - >>> 1 operand -1 +1
+    Arithmetic(binary) + - / * % >>> 
+    Increment (unary) ++ --
+    Assigment(binary) = += -= /= *= %= >>>
+    Comparision(binary) == != < > <= >=
+    Logical(binary) && || !
+    Conditional(ternary) ?: 
+
+    Reference - https://en.cppreference.com/w/c/language/operator_precedence 
+    */
+
+int main()
+{
+
+    return 0;
+}
