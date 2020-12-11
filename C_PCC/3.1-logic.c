008c4a0a2f3fd0da2c952976973a2f279222ba3c
Wallisss <visa.daho@gmail.com>
2020-12-11 07:54:56 +0200

    aaa

--- C_PCC/3.1-logic.c
+++ C_PCC/3.1-logic.c
@@ -3,6 +3,28 @@
 
 int main()
 {
-    
+    int age=18;
+    if(!(age>17))//mean !(true) = false >>> oposite 
+    {
+        printf("This is adult \n");
+    }
+    else
+    {
+        printf("This is not adult \n");
+    }
+    double money=30000;
+    bool isGraduated=true;
+    if(age>17 && money>25000 || isGraduated)//mean !(true) = false >>> oposite 
+    {
+        printf("This is rich \n");
+    }
+    else
+    {
+        printf("This is not rich \n");
+    }
+
+
+
+
     return 0;
 }
